5b24b6002561239d9d1c90a3871c25438235349d
Marcooo-Sna <mjeysiona13@gmail.com>
2025-11-13 16:50:22 -0600

    Se le piden los datos al usuario

--- Test.java
+++ Test.java
@@ -4,5 +4,15 @@ public class Test {
          public static void main(String[] args) {
         //Pido los datos al usuario
         Scanner lector = new Scanner(System.in);
+        System.out.println("Ingrese el valor de K:"):
+        double k = lector.nextDouble();
+        System.out.println("Ingrese el valor de d:"):
+        double d = lector.nextDouble();
+        System.out.println("Ingrese el valor de h:"):
+        double h = lector.nextDouble();
+        System.out.println("Ingrese el valor de l:"):
+        double l = lector.nextDouble();
+
+
      }
 }
