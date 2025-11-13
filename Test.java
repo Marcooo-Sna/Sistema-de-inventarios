03291192e3590dbe111c51fe2f89751ae0f01225
Marco De Jesús Rivero Siona <mjeysiona13@gmail.com>
2025-11-13 16:02:21 -0600

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        //Se piden los datos al usuario
+        Scanner TaylorSwiftLee = new Scanner(System.in);
+        System.out.println("Ingrese el valor de K: ");
+        double k = TaylorSwiftLee.nextDouble();
+        System.out.println("Ingrese el valor de D: ");
+        double d = TaylorSwiftLee.nextDouble();
+        System.out.println("Ingrese el valor de H: ");
+        double h = TaylorSwiftLee.nextDouble();
+        System.out.println("Ingrese el valor de L ");
+        double l = TaylorSwiftLee.nextDouble();
+    }
+}
