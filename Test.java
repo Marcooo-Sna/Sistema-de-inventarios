25b3921610667ef875f532143b110b51e0041af6
Marcooo-Sna <mjeysiona13@gmail.com>
2025-11-13 16:42:06 -0600

    Pido los datos al usuario

--- Test.java
+++ Test.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
 public class Test {
-    
+         public static void main(String[] args) {
+        //Pido los datos al usuario
+        Scanner lector = new Scanner(System.in);
+     }
 }
