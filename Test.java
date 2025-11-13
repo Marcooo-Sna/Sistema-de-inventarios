23df14dea6d1089c1a7682bc5dcabfba05e51cb6
Marcooo-Sna <mjeysiona13@gmail.com>
2025-11-13 17:11:26 -0600

    Agregue la parte donde se mostraran los resultados

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Test {
          public static void main(String[] args) {
-        //Pido los datos al usuario
+        //Le pido los datos al usuario.
         Scanner lector = new Scanner(System.in);
         System.out.println("Ingrese el valor de K:"):
         double k = lector.nextDouble();
@@ -12,7 +12,11 @@ public class Test {
         double h = lector.nextDouble();
         System.out.println("Ingrese el valor de l:"):
         double l = lector.nextDouble();
-
-
+        //Se muestran los resultados.
+        System.out.println("La cantidad de pedido es: " + cantidadpedido(k, d, h));
+        System.out.println("Duración del ciclo" + duracionciclo + (cantidadpedido(k, d, h), d));
+        System.out.println(":" + N(l,duracionciclo(cantidadpedido(d, d, h), d)));
+        System.out.println("Le:" + Le(l, N(l, duracionciclo(cantidadpedido(k, d, h), d)), duracionciclo(cantidadpedido(k, d, h), d)));
+        System.out.println("Punto de reorden" + puntodereorden(Le(l, N(l, DuracionCiclo(cantidadpedido(k, d, h), d)), duracionciclo(cantidadpedido(k, d, h), d)), d));
      }
 }
